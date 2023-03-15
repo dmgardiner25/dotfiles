8832c002418338a955b47f3ef64bce0b95706414
David Gardiner <dmgardiner25@gmail.com>
2023-03-15 18:46:08 +0000

    Add fish and csh config

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ echo hello
 
 echo "export DOTFILES_TEST=dotfiles" >> ~/.bashrc
 echo "export DOTFILES_TEST=dotfiles" >> ~/.zlogin
+echo "export DOTFILES_TEST=dotfiles" >> ~/.config/fish/config.fish
+echo "export DOTFILES_TEST=dotfiles" >> ~/.cshrc
